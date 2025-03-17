1c9471fb53b54ceae1d7a85cd81082972ea3e6bd
huangdeshun <124752289+TXAMAXT@users.noreply.github.com>
2025-03-18 04:30:51 +0800

    添加power模块的使用说明

--- modules/power_meter/power_meter.h
+++ modules/power_meter/power_meter.h
@@ -9,7 +9,9 @@ typedef struct{
     float current;
     float power;
 }power_meter_data_t;
-
+//包含power_meter.h文件后在application层对应的任务线程直接调用函数即可
+void power_meter_init(void);
+//包含power_meter.h文件后在application层定义power_meter_data_t结构体指针，对应的任务线程直接调用函数赋值指针即可
 power_meter_data_t *get_power_meter_data(void);
 
 #endif
